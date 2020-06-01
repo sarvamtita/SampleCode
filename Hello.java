db6a54a68103d31cdfc51074d543e35e4dde0a22
Sarvam Ganesh <ubuntu@ip-172-31-1-223.us-east-2.compute.internal>
2020-06-01 22:42:07 +0000

    Added a new print statement

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 //This is my First Java Program
 class Hello{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java"); 
+     System.out.println("Welcome to Spring Boot"); 
     }  
 }  
